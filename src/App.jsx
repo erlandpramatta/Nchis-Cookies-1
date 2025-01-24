cbefcbb09f4c0b7b44495c9044b09f114527c786
hilariusalan <hilariusalan28@gmail.com>
2025-01-24 08:53:57 +0700

    update routing admin

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import {BrowserRouter as Router, Routes, Route, BrowserRouter} from 'react-route
 import Navbar from './components/navbar';
 import Home from './components/home';
 import Home2 from './components/home2';
+import User from './pages/user.jsx';
 import Dashboard from './components/dashboard.jsx';
+import Admin from './pages/admin.jsx';
 import Contact from "./components/contact.jsx";
 import Footer from './components/footer';
 
@@ -12,9 +14,9 @@ function App() {
       <div className="bg-gradient-to-b from-black via-amber-950 to-black">
       <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<User/>}/>
         <Route path='/home' element={<Home2/>}/>
-        <Route path='dashboard' element={<Dashboard/>}/>
+        <Route path='Admin-Dashboard' element={<Admin/>}/>
       </Routes>
     </BrowserRouter>    
       </div>
---
+++ src/pages/user.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Home from '../components/home'
+
+const user = () => {
+  return (
+    <div>
+        <Home/>
+    </div>
+  )
+}
+
+export default user
